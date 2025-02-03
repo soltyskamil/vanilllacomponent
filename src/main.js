84bb8be97956d93e8f567e443e68c2e1caaed533
Kamil <kamils3542@gmail.com>
2025-02-03 13:16:05 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,106 +1,101 @@
-// const tabsData = [
-//   {
-//     label: 'Oferty',
-//     content: 'Telefon!!!',
-//     id: 0,
-//     active: true,
-//   },
-//   {
-//     label: 'Informacje o produkcie',
-//     content: 'To są informacje o produkcie',
-//     id: 1,
-//     active: false,
-//   },
-//   {
-//     label: 'Opinie i recenzje',
-//     content: 'Dokurwiona sprawa',
-//     id: 2,
-//     active: false,
-//   },
-//   {
-//     label: 'Zadaj pytanie',
-//     content: 'Pytanko pytanko',
-//     id: 3,
-//     active: false,
-//   },
-//   {
-//     label: 'Kup lokalnie',
-//     content: 'Kup koło chałupy',
-//     id: 4,
-//     active: false,
-//   },
-// ]
-const tabs = document.querySelector('#tabsButtons')
-const tabsContent = document.querySelector('#tabsContent')
+function loadData(container, data) {
+  const tabsButtons = document.createElement('div')
+  tabsButtons.classList.add('tabsButtons')
 
-// tabsData.forEach(data => {
-//   const dataEl = document.createElement('button')
-//   dataEl.setAttribute('data-id', data.id)
-//   dataEl.innerHTML = `${data.label}`
-//   tabs.appendChild(dataEl)
-//   if(data.active){
-//     tabsContent.innerHTML = `${data.content}`
-//   }
-//   else return
-// })
- // tabsData.map(function(e){
-    //   if(e.active && e.id != id){
-      //     e.active = !e.active
-      //     tabsData[id].active = !tabsData[id].active;
-      //     tabsContent.innerHTML = `${tabsData[id].content}`
-      //     console.log(tabsData[id], tabsData)
-      //   }
-      // })
-/*
-Wymagania techniczne:
-komponent będzie można stworzyć za pomocą klasy lub funkcji np. new Tabs(container, config) lub createTabsComponent(container, config)
-container to element html, do którego należy “wstrzyknąć” komponent zakładek
-config to dowolny obiekt lub tablica na podstawie, której należy wygenerować komponent. Przykładowy schemat: 
-const tabs = [ {label: string, content: string} ]
-domyślnie aktywna jest pierwsza zakładka
-komponent wyświetla się poprawnie na mobile oraz desktop
-*/
+  data.forEach((item, index) => {
+    const tabsInfo = document.createElement('button')
+    tabsInfo.classList.add('tabBtn')
+    tabsInfo.innerHTML = item.label
 
-/* 
-  - Stworzenie elementu HTML to którego mamy wstrzyknąć zakładki
-  - dodanie zakładek za pomocą tablicy z danymi
-  - funkcjonalność z powyższych metod
-*/
-const tabData = []
-
-function addItem(container, content){
-  tabData.push({
-    label: container,
-    content: content,
-    id: tabData.length,
-    active: false,
+    tabsButtons.appendChild(tabsInfo)
+    container.appendChild(tabsButtons)
   })
-  const button = document.createElement('button')
-  button.setAttribute('data-id', tabData.length - 1)
-  button.innerHTML = container;
-  tabData[0]['active']=true;
-  tabs.insertBefore(button, button.nextSibling)
-  tabsContent.innerHTML = `${tabData[0].content}`
-  const buttons = document.querySelectorAll('button')
-  buttons[0].style.backgroundColor = '#95a4aa'
-  buttons.forEach(function(element){
-    element.addEventListener('click', () => switchData(element))
-  })
+  showData(container, data)
 }
 
-function switchData(element){
-  const id = parseInt(element.getAttribute('data-id'))
-  const buttons = document.querySelectorAll('button')
-  tabData.map(function(e){
-    if(e.active && e.id != id){
-      e.active = !e.active
-      tabData[id].active = !tabData[id].active;
-      tabsContent.innerHTML = `${tabData[id].content}`
-      console.log(e.id, id)
-      buttons[e.id].style.backgroundColor = '#eef1f5'
-      buttons[id].style.backgroundColor = '#95a4aa'
+
+function showData(container, data){
+  const tabsConfig = document.createElement('div')
+  tabsConfig.classList.add('tabsConfig')
+
+  const buttons = document.querySelectorAll('.tabBtn')
+  buttons[0].classList.add('active')
+
+  buttons.forEach((item, index) => {
+    if(item.classList.contains('active')){
+      tabsConfig.innerHTML = data[index].info
+      container.appendChild(tabsConfig)
     }
-    
+    item.addEventListener('click', (e) => switchData(e, buttons, container, data, tabsConfig))
   })
 
 }
+
+function switchData(e, buttons, container, data, tabsConfig){
+  if(e.target.classList.contains('active')) return
+  
+  else {
+    buttons.forEach((item) => item.classList.remove('active'))
+    e.target.classList.add('active')
+  }
+
+  buttons.forEach((item, index) => {
+    if(item.classList.contains('active')){
+      tabsConfig.innerHTML = data[index].info
+      container.appendChild(tabsConfig)
+    }
+  })
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+  const parent = document.querySelector('#app')
+ 
+  const container = document.createElement('div')
+  container.id = 'tabsComponent'
+  
+  parent.appendChild(container)
+  
+  const data = [
+    {
+        label: 'Pierwsza zakładka',
+        info: 'Informacje dotyczące pierwszej zakładki',
+    },
+    {
+        label: 'Druga zakładka',
+        info: 'Informacje dotyczące drugiej zakładki',
+    },
+    {
+        label: 'Trzecia zakładka',
+        info: 'Informacje dotyczące trzeciej zakładki',
+    },
+    {
+        label: 'Czwarta zakładka',
+        info: 'Informacje dotyczące czwartej zakładki',
+    },
+    {
+        label: 'Piąta zakładka',
+        info: 'Informacje dotyczące piątej zakładki',
+    },
+    {
+        label: 'Szósta zakładka',
+        info: 'Informacje dotyczące szóstej zakładki',
+    },
+    {
+        label: 'Siódma zakładka',
+        info: 'Informacje dotyczące siódmej zakładki',
+    },
+    {
+        label: 'Ósma zakładka',
+        info: 'Informacje dotyczące ósmej zakładki',
+    },
+    {
+        label: 'Dziewiąta zakładka',
+        info: 'Informacje dotyczące dziewiątej zakładki',
+    },
+  ];
+  loadData(container, data)
+}
+)
